166fbba94b2f0e6bd7372a02fb52c3e03ba9fe75
sebastianuribe12 <urisbe@gmail.com>
2023-08-04 17:18:03 -0500

    se crean objetos para sumar cuantos objetos llevara el usuario

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const items =document.getElementById('items')
 const templateCard = document.getElementById('template-card').content
 const fragment = document.createDocumentFragment()
+let carrito ={}
 
 document.addEventListener('DOMContentLoaded', () =>{
     fetchData()
+})
+items.addEventListener('click', e =>{
+    addCarrito (e)
 })
 const fetchData = async () => {
     try{
@@ -29,4 +33,29 @@ const pintarCards = data => {
         fragment.appendChild(clone)
     })
     items.appendChild(fragment)
+}
+
+const addCarrito = e => {
+    //console.log(e.target)
+    //console.log(e.target.classList.contains('btn-dark'))
+    if(e.target.classList.contains('btn-dark')){
+        setCarrito(e.target.parentElement)
+    }
+    e.stopPropagation()
+}
+
+const setCarrito = objeto =>{
+    console.log(objeto)
+    const producto = {
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        title: objeto.querySelector('h5').textContent,
+        precio: objeto.querySelector('p').textContent,
+        cantidad: 1
+    }
+
+    if(carrito.hasOwnProperty(producto.id)){
+        producto.cantidad = carrito[producto.id].cantidad + 1
+    }
+     carrito[producto.id] = {...producto}
+    console.log(producto)
 }
